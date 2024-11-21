8d86be3766816822b66ad6837edd31685d27a484
Geno <2720203124@qq.com>
2024-11-22 00:59:36 +0800

    project initialization

---
+++ trading-common/src/main/java/com/trade/constant/JwtClaimsConstant.java
@@ -0,0 +1,6 @@
+package com.trade.constant;
+
+public class JwtClaimsConstant {
+
+    public static final String ID = "id";
+}
---
+++ trading-common/src/main/java/com/trade/constant/MessageConstant.java
@@ -0,0 +1,7 @@
+package com.trade.constant;
+
+
+public class MessageConstant {
+    public static final String EMAIL_RECEIVE_NULL = "邮件收信人不能为空！";
+    public static final String EMAIL_CONTENT_NULL = "邮件内容不能为空！";
+}
